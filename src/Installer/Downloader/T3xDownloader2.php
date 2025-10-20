523dc051fa916f4fa42f63fe55ba27b7da590518
lizbNet <96036646+lizbNet@users.noreply.github.com>
2025-10-20 14:53:53 +0200

    Update T3xDownloader2.php
    Signature compatible with Composer ≥ 2.3

--- src/Installer/Downloader/T3xDownloader2.php
+++ src/Installer/Downloader/T3xDownloader2.php
@@ -29,9 +29,9 @@ class T3xDownloader2 extends ArchiveDownloader implements ChangeReportInterface
     /**
      * {@inheritDoc}
      */
-    protected function extract(PackageInterface $package, $file, $path)
+    protected function extract(PackageInterface $package, string $file, string $path): \React\Promise\PromiseInterface
     {
-        T3xDownloaderUtility::extract($package, $file, $path);
+        return \React\Promise\resolve(T3xDownloaderUtility::extract($package, $file, $path));
     }
 
     /**
