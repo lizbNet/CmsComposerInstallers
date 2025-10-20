878975432aff91502f27afd2674beefe6e24bd26
lizbNet <96036646+lizbNet@users.noreply.github.com>
2025-10-20 16:03:26 +0200

    Update T3xDownloader2.php

--- src/Installer/Downloader/T3xDownloader2.php
+++ src/Installer/Downloader/T3xDownloader2.php
@@ -29,7 +29,7 @@ class T3xDownloader2 extends ArchiveDownloader implements ChangeReportInterface
     /**
      * {@inheritDoc}
      */
-    private function extract(PackageInterface $package, string $file, string $path): \React\Promise\PromiseInterface
+    protected function extract(PackageInterface $package, string $file, string $path): \React\Promise\PromiseInterface
     {
         return \React\Promise\resolve(T3xDownloaderUtility::extract($package, $file, $path));
     }
